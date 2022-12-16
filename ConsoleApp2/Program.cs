506f0ba15e47cf1e5bbc722b7707846e0ef19e8d
Hassan Habib <hassanhabib@live.com>
2022-12-15 23:55:41 -0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuzzyFuzzDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var broker = new StorageBroker();
+
+            IQueryable<Student> students =
+                broker.Students.Where(student =>
+                    StorageBroker.SoundsLike(student.Name) == StorageBroker.SoundsLike("Ibrahem"));
+
+            Console.Write(students.Count());
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public class StorageBroker : DbContext
+    {
+        public StorageBroker() => this.Database.Migrate();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=DemoDB;");
+
+        [DbFunction(Name = "SOUNDEX", IsBuiltIn = true)]
+        public static string SoundsLike(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
